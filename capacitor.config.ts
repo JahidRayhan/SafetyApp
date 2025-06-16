b8fbaccdce66074bcd32f1a0e99fa0608dc8a2b1
JahidRayhan <106985800+JahidRayhan@users.noreply.github.com>
2025-06-16 13:31:02 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,37 +1,20 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.42871f4c4d7240dba5b25777dc7760eb',
+  appId: 'com.safeguard.app',
   appName: 'SafeGuard',
   webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    url: 'https://42871f4c-4d72-40db-a5b2-5777dc7760eb.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+
   plugins: {
     SplashScreen: {
       launchShowDuration: 0
-    },
-    App: {
-      launchUrl: 'https://42871f4c-4d72-40db-a5b2-5777dc7760eb.lovableproject.com?forceHideBadge=true'
-    },
-    PushNotifications: {
-      presentationOptions: ["badge", "sound", "alert"]
     }
   },
   android: {
     buildOptions: {
-      keystorePath: null,
-      keystoreAlias: null
-    },
-    allowMixedContent: true
-  },
-  ios: {
-    scheme: 'SafeGuard',
-    contentInset: 'automatic',
-    backgroundColor: '#ffffffff'
+      keystorePath: undefined,
+      keystoreAlias: undefined
+    }
   }
 };
 
