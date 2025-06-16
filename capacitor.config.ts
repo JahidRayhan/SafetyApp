37d2f8755ce67713cb80058c180ebd28626ab9ae
MdAsrafAli <107364717+MdAsrafAli@users.noreply.github.com>
2025-06-16 13:45:27 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,12 +8,6 @@ const config: CapacitorConfig = {
   plugins: {
     SplashScreen: {
       launchShowDuration: 0
-    },
-    App: {
-      launchUrl: 'https://42871f4c-4d72-40db-a5b2-5777dc7760eb.lovableproject.com?forceHideBadge=true'
-    },
-    PushNotifications: {
-      presentationOptions: ["badge", "sound", "alert"]
     }
   },
   android: {
